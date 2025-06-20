6229bc24f7a65d4c8709f1cccc2755bee726236b
TJ Paz <tj24gg@gmail.com>
2025-06-21 00:00:35 +0800

    Auto-install w/ Treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,11 @@
 return {
-    { "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate" }
+    { "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup {
+            ensure_installed = { "nu", "rust", },
+            highlight = { enable = true, },
+            auto_install = true,
+        }
+    end
+    }
 }
