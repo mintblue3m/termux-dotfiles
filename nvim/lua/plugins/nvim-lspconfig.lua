2cfc4ad009b77a28bb46177fc0970e8c917e9d8b
TJ Paz <tj24gg@gmail.com>
2025-06-20 23:59:09 +0800

    Add nushell to LSP config

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -19,6 +19,7 @@ return {
             local capabilities = require("blink.cmp").get_lsp_capabilities()
             require("lspconfig").lua_ls.setup { capabilities = capabilities }
             require("lspconfig").rust_analyzer.setup { capabilities = capabilities }
+            require("lspconfig").nushell.setup { capabilities = capabilities }
         end,
     }
 }
