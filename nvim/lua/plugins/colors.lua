323fa6027f58e23a7a2875cffba41ec2ca0479ac
TJ Paz <tj24gg@gmail.com>
2025-06-22 16:20:40 +0800

    Change Neovim colors to zenbones
    use duckbones theme within zenbones

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,16 +1,12 @@
 return {
     {
-    -- {
-        --     "bettervim/yugen.nvim",
-        --     config = function()
-            --         vim.cmd.colorscheme("yugen")
-            --     end,
-            -- }
-            {
-                "nyoom-engineering/oxocarbon.nvim",
-                config = function()
-                    vim.cmd.colorscheme("oxocarbon")
-                end,
-            },
-        }
-    }
+        "zenbones-theme/zenbones.nvim",
+        dependencies = "rktjmp/lush.nvim",
+        lazy = false,
+        priority = 1000,
+        -- you can set set configuration options here
+        config = function()
+            vim.cmd.colorscheme('duckbones')
+        end
+    },
+}
