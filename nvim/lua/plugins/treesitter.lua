61778f131cfd895c9c1bbd0c55993122fcdf790f
TJ Paz <tj24gg@gmail.com>
2025-06-23 18:36:37 +0800

    Add C, Markdown and vim/vimdoc
    And disable auto-install

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,10 +2,10 @@ return {
     { "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup {
-            ensure_installed = { "nu", "rust", },
+            ensure_installed = { "nu", "rust", "markdown", "c", "vim", "vimdoc"},
             highlight = { enable = true, },
-            auto_install = true,
+            auto_install = false,
         }
     end
-    }
+}
 }
