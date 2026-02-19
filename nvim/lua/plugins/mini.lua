ed8a1efb230906c6edd93c96f399bece1e775172
TJ Paz <tj24gg@gmail.com>
2026-02-20 05:36:06 +0800

    Fix mini.snippets implementation

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,11 +1,25 @@
 return {
     {
         "echasnovski/mini.nvim", version = false,
+        dependencies = { "rafamadriz/friendly-snippets"},
         config = function()
             require("mini.statusline").setup()
             require("mini.pairs").setup()
             require("mini.move").setup()
             require("mini.surround").setup()
+
+            local gen_loader = require('mini.snippets').gen_loader
+            event = "InsertEnter",
+            require('mini.snippets').setup({
+                snippets = {
+                    -- Load custom file with global snippets first (adjust for Windows)
+                    gen_loader.from_file('~/.config/nvim/snippets/global.json'),
+
+                    -- Load snippets based on current language by reading files from
+                    -- "snippets/" subdirectories from 'runtimepath' directories.
+                    gen_loader.from_lang(),
+                },
+            })
         end,
     }
 }
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,15 +1,4 @@
 return {
-  { -- do read the installation section in the readme of mini.snippets!
-    "echasnovski/mini.snippets",
-    dependencies = "rafamadriz/friendly-snippets",
-    event = "InsertEnter", -- don't depend on other plugins to load...
-    -- :h MiniSnippets-examples:
-    opts = function()
-      local snippets = require("mini.snippets")
-      return { snippets = { snippets.gen_loader.from_lang() }}
-    end,
-  },
-
   { -- do read the installation section in the readme of nvim-cmp!
     "hrsh7th/nvim-cmp",
     main = "cmp",
